d3b8431f477f7742d21084ac3ab9c7bb5841beb7
Raviteja <rteja2346@gmail.com>
2025-07-14 20:50:12 -0500

    Initial Commit

---
+++ SoloTravelSoul/SoloTravelSoulApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloTravelSoulApp.swift
+//  SoloTravelSoul
+//
+//  Created by Raviteja Vemulapelli on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SoloTravelSoulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoloTravelSoulTests/SoloTravelSoulTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoloTravelSoulTests.swift
+//  SoloTravelSoulTests
+//
+//  Created by Raviteja Vemulapelli on 7/14/25.
+//
+
+import Testing
+@testable import SoloTravelSoul
+
+struct SoloTravelSoulTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
